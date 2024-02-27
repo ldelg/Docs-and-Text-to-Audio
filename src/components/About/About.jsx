0d39c82c5159106fb3e2cebe8c76789126a1f0d9
Luca Del Grosso <ldelg@proton.me>
2024-02-27 18:12:31 +0100

    styling for mobile mostly

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="description">
+        <p>Welcome to our free speech-to-text and text-to-speech platform.</p>
+        <p>
+          Our tools are crafted for a wide array of applications, from
+          transcribing educational lectures and professional meetings to
+          converting written content into audible formats for easier consumption
+          and accessibility. Our services support everyone from students and
+          professionals to content creators and individuals with disabilities,
+          facilitating seamless content creation, study aids, accessible
+          reading, and multitasking opportunities.
+        </p>
+        <p>
+          While we offer our services for free, donations
+          are greatly appreciated and help us keep our platform up-to-date and
+          accessible to all. Thank you for supporting our mission to make
+          digital communication more accessible and efficient.
+        </p>
+      </div>
+      <div className="container">
+        <a
+          href="https://donate.stripe.com/4gweYZ26zbfY25y9AA"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="donateButton"
+        >
+          Donate via Stripe
+        </a>
+        <p>For crypto donations, please use the following addresses: </p>
+        <ul>
+          <li>Bitcoin (BTC): bc1qy7nqszskfp47ds4a6hpzjr6gdlg48l8rq4zw5z</li>
+          <li>
+            Ethereum, ERC20 tokens, Polygon Chain and BNB chain (ETH):
+            0xce439fbAde1F7a41C69A7c18cD1e0BE3807CfA86
+          </li>
+        </ul>
+        <div className="warning">
+          <p>
+            But first, please make sure you understand the dynamic of crypto
+            transactions (non-reverseable) and to use the matching chain
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,16 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import MobileNavbar from "./MobileNavbar/MobileNavbar";
 import { useMediaQuery } from "@react-hook/media-query";
+import AboutIcon from "./MobileNavbar/icons/about.png";
+import Speech2TextIcon from "./MobileNavbar/icons/speech2text-icon.png";
+import Text2SpeechIcon from "./MobileNavbar/icons/text2speech-icon.png";
+
 
 const Navbar = () => {
   const links = [
-    { to: "/", text: "Text2Speech", icon: "", alt: "Home icon" },
-    { to: "/Speech2Text", text: "Speech2Text", icon: "", alt: "" },
-    { to: "/About", text: "About", icon: "", alt: "About Icon" },
+    { to: "/", text: "Text2Speech", icon: Text2SpeechIcon, alt: "Text 2 Speech icon" },
+    { to: "/Speech2Text", text: "Speech2Text", icon: Speech2TextIcon, alt: "Speech 2 Text icon" },
+    { to: "/About", text: "About", icon: AboutIcon, alt: "About Icon" },
   ];
 
   const [click, setClick] = useState(false);
